9bd9e3c7eed7ef85b9c2dfb3a3d0cca919119447
lucas-s-a <lucassalves485@outlook.com>
2023-02-12 17:08:52 -0300

    atualizações rotas 2

--- public/func/calculopedido.js
+++ public/func/calculopedido.js
@@ -6,7 +6,26 @@ const ddpdds = document.querySelectorAll("#ddpedidos")
 
 function Verpedido(){
     listadecompras = []
-    //Nome
+    pedidosnumero = []
+    console.log(dd[0].children[28].childNodes)
+    console.log((dd[0].children[28].childNodes).length)
+    const dpv = (dd[0].children[28].childNodes).length
+    let x=1
+    while(x<dpv){
+        pedidosnumero.push(parseInt(dd[0].children[28].childNodes[x].value))
+        x = x+2
+    }
+    console.log(pedidosnumero)
+    if(pedidosnumero.length === 0){
+        npedd = 1
+    }else if(pedidosnumero.length > 0){
+        var max = Math.max(...pedidosnumero);
+        npedd=max+1
+    }
+    console.log(npedd)
+    const numberped = npedd
+    //console.log(dd[0].children[t2].childNodes[1].value)
+    //Nome dos produtos
     let p1 = dd[0].childNodes[1].innerText
     let p2 = dd[0].childNodes[7].innerText
     let p3 = dd[0].childNodes[13].innerText
@@ -16,7 +35,7 @@ function Verpedido(){
     let p7 = dd[0].childNodes[37].innerText
     let p8 = dd[0].childNodes[43].innerText
     let p9 = dd[0].childNodes[49].innerText
-    console.log(p1)
+    /*console.log(p1)
     console.log(p2)
     console.log(p3)
     console.log(p4)
@@ -25,7 +44,7 @@ function Verpedido(){
     console.log(p7)
     console.log(p8)
     console.log(p9)
-
+*/
 
     //Preço
     let prc1 = parseFloat(dd[0].childNodes[3].innerText)
@@ -68,6 +87,7 @@ function Verpedido(){
     console.log(qtd9)
     const total = qtd1*prc1+qtd2*prc2+qtd3*prc3+qtd4*prc4+qtd5*prc5+qtd6*prc6+qtd7*prc7+qtd8*prc8+qtd9*prc9
     console.log(total)
+
     if(qtd1 != 0){
         const l1 = {Produto:`${p1}`,PriceU:`${prc1}`,Quantidade:`${qtd1}`}
         listadecompras.push(l1)
@@ -112,6 +132,7 @@ function Verpedido(){
     let html2 = imprimir.lc.map( lar =>{
         return `<div><input name="prodcompra" type="hidden" value="${lar.Produto}"><input name="priceunit" type="hidden" value="${lar.PriceU}"><input name="qtdcompra" type="hidden" value="${lar.Quantidade}"></div>`
     })
+    document.querySelector("#npededio").innerHTML = `<div><input name="npedd" type="hidden" value="${numberped}"</div>`
     document.querySelector("#vercompra").innerHTML = html.join("")
     document.querySelector("#mandardadoscompra").innerHTML = `<div>${html2}</div>`
     document.querySelector("#valortotal").innerHTML = `<div><input name="totalcompra" type="hidden" value="${total}"><br><p>${total}</p></div>`
